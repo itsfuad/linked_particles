28061c083773c501c6041650e58c51fabe6523e1
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-10-14 21:30:52 +0600

    ..

--- particle.js
+++ particle.js
@@ -16,18 +16,48 @@ window.addEventListener('resize', function(){
 const mouse = {
     x: undefined,
     y: undefined,
-    radius: 100
+    radius: 80,
+    speed: 0,
+    weight: 1
 }
 
 //find the cursor position on the canvas from mouse and touch events
 window.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
+
+    mouse.speed = {
+        x: event.movementX,
+        y: event.movementY
+    }
+
+    particles.forEach(particle => {
+            //distance between mouse and particle
+            let dx = mouse.x - particle.x;
+            let dy = mouse.y - particle.y;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+
+            //if the distance is less than the mouse radius, move the particle
+            if(distance <= mouse.radius){
+                //x, y position required to have a distance of mouse.radius from the mouse
+                let angle = Math.atan2(dy, dx);
+                let x = mouse.x + Math.cos(angle) * mouse.radius;
+                let y = mouse.y + Math.sin(angle) * mouse.radius;
+
+                particle.x += (particle.x - x) * 0.01;
+                particle.y += (particle.y - y) * 0.01;
+            }
+    });
 });
 
 window.addEventListener('touchmove', function(event){
     mouse.x = event.touches[0].clientX;
     mouse.y = event.touches[0].clientY;
+
+    mouse.speed = {
+        x: event.movementX,
+        y: event.movementY
+    }
 });
 
 window.addEventListener('touchstart', function(event){
@@ -67,25 +97,33 @@ function generatePosition(size){
 
 
 class Particle{
-    constructor(x, y, size, color, speed){
+    constructor(x, y, size, color, weight){
+        const directionX = (Math.random() * 2 - 1);
+        const directionY = (Math.random() * 2 - 1);
         this.x = x;
         this.y = y;
         this.baseX = this.x;
         this.baseY = this.y;
         this.size = size;
-        this.color = color;
-        this.speed = {x: speed.x, y: speed.y};
+        this.opacity = 0;
+        this.weight = weight;
+        this.col = color;
+        this.speed = {x: directionX / weight, y: directionY / weight};
     }
 
     draw(){
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = `rgba(${this.col},${this.opacity})`;
         ctx.fill();
     }
 
     update(){
 
+        //gradually increase opacity
+        if(this.opacity < 1){
+            this.opacity += 0.01;
+        }
         
         this.x += this.speed.x;
         this.y += this.speed.y;
@@ -93,19 +131,14 @@ class Particle{
 
         //check mouse position and move the particle
         if(mouse.x && mouse.y){
-            //draw circle around the mouse
 
-            /*
-            //set color to transparent white
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.002)';
-
+            //show mouse radius
+            
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = `rgba(55, 200, 255, 0.02)`;
             ctx.beginPath();
             ctx.arc(mouse.x, mouse.y, mouse.radius, 0, Math.PI * 2);
-            ctx.fill();
-
-            ctx.fillStyle = 'white';
-
-            */
+            ctx.stroke();
 
             //distance between mouse and particle
             let dx = mouse.x - this.x;
@@ -116,43 +149,98 @@ class Particle{
             if(distance <= mouse.radius){
                 //x, y position required to have a distance of mouse.radius from the mouse
                 let angle = Math.atan2(dy, dx);
-                let x = mouse.x + Math.cos(angle) * mouse.radius;
-                let y = mouse.y + Math.sin(angle) * mouse.radius;
+                const speed = mouse.speed.x ** 2 + mouse.speed.y ** 2;
+                const force = (mouse.radius - distance) * mouse.weight * speed;
+                const x = Math.cos(angle) * force;
+                const y = Math.sin(angle) * force;
 
-                //if particle is inside the mouse radius, move it to the edge of the mouse radius
-                if(distance < mouse.radius - 10){
-                    this.x -= (x - this.x) * 0.1;
-                    this.y -= (y - this.y) * 0.1;
-                }else{
-                
-                    //move the particle along the curve
-                    this.x -= (x - this.x) * 0.005;
-                    this.y -= (y - this.y) * 0.005;
-                }
+                this.x -= x;
+                this.y -= y;
+            }
+
+        }
+
+        //if particle is out of canvas, randomize its position
+        if(this.x - this.size > canvas.width || this.x + this.size < 0){
+            this.opacity = 0;
+
+            this.x = generatePosition(this.size).x;
+            this.speed.x = (Math.random() * 2 - 1) / this.weight;
+            return;
+        }
+        
+        if(this.y - this.size > canvas.height || this.y + this.size < 0){
+            this.opacity = 0;
+
+            this.y = generatePosition(this.size).y;
+            this.speed.y = (Math.random() * 2 - 1) / this.weight;
+            return;
+        }
+
+        //detect collision with other particles
+        for(let i = 0; i < particles.length; i++){
+            if(this === particles[i]) continue;
+
+            if(distance(this.x, this.y, particles[i].x, particles[i].y) < this.size + particles[i].size){
+                resolveCollision(this, particles[i]);
             }
         }
-
-        if(this.x + this.size > canvas.width || this.x - this.size < 0){
-            //this will reverse the speed of the particle
-            //this.speed.x *= -1;
-
-            //this will make the particle disappear and reappear on a random x position
-            this.x = generatePosition(this.size).x;
-            const directionX = (Math.random() * 2 - 1);
-            this.speed.x = (2/this.size) * directionX;
-        }
-
-        if(this.y + this.size > canvas.height || this.y - this.size < 0){
-            //this will reverse the speed of the particle
-            //this.speed.y *= -1;
-
-            //this will make the particle disappear and reappear on a random x position
-            this.y = generatePosition(this.size).y;
-            const directionY = (Math.random() * 2 - 1);
-            this.speed.y = (2/this.size) * directionY;
-        }
-
     }
+}
+
+function resolveCollision(particle, otherParticle){
+    const xVelocityDiff = particle.speed.x - otherParticle.speed.x;
+    const yVelocityDiff = particle.speed.y - otherParticle.speed.y;
+
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    //prevent accidental overlap of particles
+    if(xVelocityDiff * xDist + yVelocityDiff * yDist >= 0){
+
+        //get the angle between the two particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        //store mass in var for better readability in collision equation
+        const m1 = particle.weight;
+        const m2 = otherParticle.weight;
+
+        //velocity before equation
+        const u1 = rotate(particle.speed.x, particle.speed.y, angle);
+        const u2 = rotate(otherParticle.speed.x, otherParticle.speed.y, angle);
+
+        //velocity after 1d collision equation
+        const v1 = {x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y};
+        const v2 = {x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y};
+
+        //final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1.x, v1.y, -angle);
+        const vFinal2 = rotate(v2.x, v2.y, -angle);
+
+        //swap particle velocities for realistic bounce effect
+        particle.speed.x = vFinal1.x;
+        particle.speed.y = vFinal1.y;
+
+        otherParticle.speed.x = vFinal2.x;
+        otherParticle.speed.y = vFinal2.y;
+    }
+}
+
+
+function rotate(x, y, angle){
+    const rotatedVelocities = {
+        x: x * Math.cos(angle) - y * Math.sin(angle),
+        y: x * Math.sin(angle) + y * Math.cos(angle)
+    };
+
+    return rotatedVelocities;
+}
+
+function distance(x1, y1, x2, y2){
+    const xDistance = x2 - x1;
+    const yDistance = y2 - y1;
+
+    return Math.sqrt(xDistance ** 2 + yDistance ** 2);
 }
 
 function init(){
@@ -162,26 +250,18 @@ function init(){
 
     for(let i = 0; i < particleCount; i++){
 
-        const size = Math.random() * 2 + 1;
+        const size = Math.random() * 3 + 1;
         const {x, y} = generatePosition(size);
-        //randomly choose -1 or 1
-        const directionX = (Math.random() * 2 - 1);
-        const directionY = (Math.random() * 2 - 1);
-        const color = 'white';
 
-        particles.push(new Particle(x, y, size, color, { x: (2/size) * directionX, y: (2/size) * directionY}));
+        const color = '255,255,255';
+        const weight = 2/size;
+
+        particles.push(new Particle(x, y, size, color, weight));
     }
 }
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for(let i = 0; i < particles.length; i++){
-        particles[i].update();
-        particles[i].draw();
-    }
-
-    
     //connect closer particles with lines
     for(let i = 0; i < particles.length; i++){
         for(let j = i; j < particles.length; j++){
@@ -189,19 +269,21 @@ function animate(){
             const dy = particles[i].y - particles[j].y;
             const distance = Math.sqrt(dx * dx + dy * dy);
 
-            if(distance < 100){
+            if(distance < 200){
                 ctx.beginPath();
-                ctx.strokeStyle = `rgba(55, 200, 255, ${1 - distance / 100})`;
-                ctx.lineWidth = 1 - (distance / 100);
+                ctx.strokeStyle = `rgba(55, 200, 255, ${2 - distance / 100})`;
+                ctx.lineWidth = 2 - (distance / 100);
                 ctx.moveTo(particles[i].x, particles[i].y);
                 ctx.lineTo(particles[j].x, particles[j].y);
                 ctx.stroke();
             }
         }
     }
-    
-    
 
+    particles.forEach(particle => {
+        particle.draw();
+        particle.update();
+    });
     requestAnimationFrame(animate);
 }
 
